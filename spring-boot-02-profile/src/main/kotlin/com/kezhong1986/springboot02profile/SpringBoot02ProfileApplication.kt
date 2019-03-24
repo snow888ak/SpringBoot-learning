ceca432a9e71d361eaeb330512ed76bdfc507280
kezhong1986 <396926020@qq.com>
2019-03-24 22:30:18 +0800

    测试profiles

---
+++ spring-boot-02-profile/src/main/kotlin/com/kezhong1986/springboot02profile/SpringBoot02ProfileApplication.kt
@@ -0,0 +1,11 @@
+package com.kezhong1986.springboot02profile
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBoot02ProfileApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBoot02ProfileApplication>(*args)
+}
---
+++ spring-boot-02-profile/src/test/kotlin/com/kezhong1986/springboot02profile/SpringBoot02ProfileApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.kezhong1986.springboot02profile
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringBoot02ProfileApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
